f0fe247b8907c53ef30119e94944c24e0ddc94e4
rcovert-20 <71655825+rcovert-20@users.noreply.github.com>
2020-09-21 13:31:12 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+var boxEle = document.body.querySelector(".box");
+
+var numberOne = prompt("First number");
+
+var numberTwo = prompt("Second number");
+
+var x = numberOne;
+
+var y = numberTwo;
+
+var z = x + y;
+
+boxEle.innerHTML=numberOne+numberTwo;
+
+boxEle.innerHTML=userInput;
